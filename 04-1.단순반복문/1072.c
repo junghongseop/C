a512eda62e7fe71abc0df86b51bf6dfb9270a4c2
정홍섭 <126876363+junghongseop@users.noreply.github.com>
2023-08-19 20:21:15 +0900

    Update 1072.c

--- 04-1.단순반복문/1072.c
+++ 04-1.단순반복문/1072.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    for(int i=0; i<a; i++)
+    {
+    	scanf("%d",&b);
+    	printf("%d\n", b);
+	}
+    
+    return 0;
+}
 
